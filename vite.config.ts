7fab2dfb61400527cbc7aafb7a0cdc3fd112422a
ics-matsumoto <matsumoto@ics-web.jp>
2025-08-21 14:33:45 +0900

    base変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
-  base: "/250821_loro_crdt/", // GitHub Pages用のベースパス
+  base: "./", // GitHub Pages用のベースパス
 });
